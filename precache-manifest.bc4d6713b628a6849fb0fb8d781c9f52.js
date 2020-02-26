48da228eb3fc841ea2a64833cd5df5aeec716502
deep0122 <deeppatel0122@gmail.com>
2020-02-26 00:12:04 -0500

    Updates

---
+++ precache-manifest.bc4d6713b628a6849fb0fb8d781c9f52.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "691c53d6c071df376fb83e638ffaa6c3",
+    "url": "/bookfinder-chingu/index.html"
+  },
+  {
+    "revision": "28dcfbd36f2382929291",
+    "url": "/bookfinder-chingu/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "928f63800aeace66a80e",
+    "url": "/bookfinder-chingu/static/js/2.c86b3a1b.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/bookfinder-chingu/static/js/2.c86b3a1b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "28dcfbd36f2382929291",
+    "url": "/bookfinder-chingu/static/js/main.2f65534d.chunk.js"
+  },
+  {
+    "revision": "5b9e890373cda4b86694",
+    "url": "/bookfinder-chingu/static/js/runtime-main.a697575d.js"
+  }
+]);
